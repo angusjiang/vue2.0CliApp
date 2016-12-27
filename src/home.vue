2c071aaed89bd890579d2d0ba63bb5e3adc873f7
jiangxujian <1270923064@qq.com>
2016-12-27 23:02:29 +0800

    infinatescoll

--- src/home.vue
+++ src/home.vue
@@ -37,19 +37,14 @@
 			</mt-header>
 		</div>
 		<div class="app-content">
-			<div v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="50" infinite-scroll-immediate-check="true" ref="wrapper" :style="{ height: wrapperHeight + 'px' }">
-				<ul >
-				  <li style="height:40px;" v-for="item in list">
-				  	<router-link to="/home/detail/123">
-				  	{{ item }} click me go detail
-				  	</router-link>
-				  </li>
-				</ul>
-				
-			</div>
-			<div v-show="loading" class="page-infinite-loading start-box box-align">
+			<div class="page-infinite-wrapper" ref="wrapper" :style="{ height: wrapperHeight + 'px' }">
+		      <ul class="page-infinite-list" v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="50" infinite-scroll-immediate-check="false"  infinite-scroll-listen-for-event="needNow()">
+		        <li v-for="(item, index) in articles" class="page-infinite-listitem">{{index}}{{ item.title }}</li>
+		      </ul>
+		      <p v-show="loading" class="page-infinite-loading" :style="{'marginBottom':'55px'}">
 		        <mt-spinner type="fading-circle"></mt-spinner>
 		        加载中...
+		      </p>
 		    </div>
 		</div>
 		<div class="footer-nav">
@@ -66,7 +61,7 @@
 				loading: false,
 				list: [],
 				articles:[],
-				page_size: 16,
+				page_size: 15,
 				page_no: 1,
 				wrapperHeight: 0
 			}
@@ -74,51 +69,54 @@
 		components: {
 			navigtor
 		},
-		mounted() {
-	      this.wrapperHeight = document.documentElement.clientHeight - this.$refs.wrapper.getBoundingClientRect().top;
-	      for (let i = 1; i <= 20; i++) {
-	        this.list.push(i);
-	      }
+		mounted() { //初始计算时请求数据接口
+	      this.wrapperHeight = document.documentElement.clientHeight - this.$refs.wrapper.getBoundingClientRect().top - 55;
+	      this.initF();
 	    },
 		methods: {
 			loadMore() {
 				var that = this;
-		        this.loading = true;
-		        setTimeout(() => {
-		          let last = that.list[that.list.length - 1];
-		          for (let i = 1; i <= 10; i++) {
-		            that.list.push(last + i);
-		          }
-		          that.loading = false;
-		        }, 2500);
+				//监听loadmore事件，设置延迟效果
+				setTimeout(() => {
+					//开始加载数据过程打开加载图标，关闭loadmore事件
+					that.loading = true;
+			        that.page_no++;
+			        that.initF(true);
+				}, 700);
+		    },
+		    needNow() {
+
 		    },
 			initF(isAdd) {
-				var _ = this;
+				var that = this;
+				//请求前关闭loaddmore事件，显示加载图标
 				this.loading = true;
 				this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=' + this.page_size + '&start='+this.page_no, {}, {
 			        headers: {
 			        },
 			        emulateJSON: true
 			    }).then(function(response) {
-			    	this.loading = false;
+			    	//请求成功后loaddmore事件再次开启，隐藏加载图标
+			    	that.loading = false;
 			    	if (isAdd) {
 			    		this.articles = this.articles.concat(response.data.subjects);
 			    	} else {
 			    		this.articles = response.data.subjects;
 			    	}
-			        
-			        // this.loadMore();
 			    }, function(response) {
 			        console.log(response)
 			    });
 			}
 		},
 		created() {
+			this.needNow();
 		    // this.initF();
 		    // console.log(router);
 		}
 	}
 </script>
 <style scope>
-
+	li{
+		height: 50px;
+	}
 </style>
